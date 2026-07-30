3a4da727bef75cfe8d0fe35869dad5db10fac746
joann15 <jmm4860@rit.edu>
2026-07-30 14:41:08 +0400

    Fix Run Connection

--- app.py
+++ app.py
@@ -1645,4 +1645,9 @@ def download():
 # RUN
 # ---------------------------
 if __name__ == "__main__":
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
